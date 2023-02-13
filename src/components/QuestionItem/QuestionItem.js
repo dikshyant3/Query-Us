27a24066b76aad265edb24d38feb701b0a4f777b
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-02-13 23:57:05 +0545

    UI updated

--- src/components/QuestionItem/QuestionItem.js
+++ src/components/QuestionItem/QuestionItem.js
@@ -1,19 +1,79 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
+import "./QuestionItem.css";
+import axios from "axios";
+import { Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const QuestionItem = (props) => {
-    const [title,setTitle]=useState('');
-    const [views,setViews]=useState();
-    const [count,setCount]=useState();
+const QuestionItem = () => {
+  const [questions, setQuestions] = useState([]);
+  const getAllQuestions = async () => {
+    const url = "https://queryus-production.up.railway.app/question/all";
+    try {
+      const res = await axios.get(url, { withCredentials: true });
+      console.log(res.data);
+      setQuestions(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllQuestions();
+  }, []);
+
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="card-title">{title}</div>
-        <div className="card-count">
-            <p>{views}</p>
-            <p>{count}</p>
-        </div>
+    <>
+      <div className="card">
+        {questions.map((question) => (
+          <div className="question-container" key={question.id}>
+            <div className="question-tags">
+              {question.tags.map((tag) => (
+                <span className="tag">{tag}</span>
+              ))}
+            </div>
+            <div className="question-container-up">
+              <div className="question-body">
+                <div className="question-title">
+                  <Link>
+                    <p>{question.questionTitle}</p>
+                  </Link>
+                </div>
+
+                <div className="question-container-down">
+                  <div className="question-stats">
+                    <div className="question-stat">
+                      <p>{question.answerCount}</p>
+
+                      <span>answers</span>
+                    </div>
+                    <div className="question-stat">
+                      <p>{question.voteCount}</p>
+                      <span>upvotes</span>
+                    </div>
+                    <div className="question-stat">
+                      <p>{question.views}</p>
+                      <span>views</span>
+                    </div>
+                    <div className="author">
+                      <small className="timestamp">
+                        {question.timestamp}
+                        {/* {`${(question.timestamp).toLocaleDateString()}`} */}
+
+                        {/* timestamp need to be converted in locale string */}
+                      </small>
+                      <div className="author-details">
+                        {/* Avatar of user */}
+                        <Avatar />
+                        <p>{question.userId}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
