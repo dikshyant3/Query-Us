eb20902f3070e6ba640859387160ceef3c254079
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-02-20 23:43:07 +0545

    pagination updated

--- src/components/QuestionItem/QuestionItem.js
+++ src/components/QuestionItem/QuestionItem.js
@@ -1,91 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./QuestionItem.css";
-import axios from "axios";
 import { Avatar } from "@mui/material";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import { Link } from "react-router-dom";
 
-const QuestionItem = () => {
-  const [questions, setQuestions] = useState([]);
-  // const[currentPage,setCurrentPage]=useState(1);
-
-  const getAllQuestions = async () => {
-    const url = "https://queryus-production.up.railway.app/question/all";
-    try {
-      const res = await axios.get(url, { withCredentials: true });
-      console.log(res.data);
-      setQuestions(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getAllQuestions();
-  }, []);
-
+const QuestionItem = ({ question }) => {
   return (
     <>
       <div className="card">
-        {questions.map((question) => (
-          <div className="question-container" key={question.id}>
-            <div className="question-container-left">
-              <div className="question-sidebar">
-                <div className="sidebar-content">
-                  <Avatar />
-                </div>
-                <div className="sidebar-content">
-                  <ExpandLessIcon />
-                  <p>{question.voteCount}</p>
-                </div>
+        <div className="question-container" key={question.id}>
+          <div className="question-container-left">
+            <div className="question-sidebar">
+              <div className="sidebar-content">
+                <Avatar />
+              </div>
+              <div className="sidebar-content">
+                <ExpandLessIcon />
+                <p>{question.voteCount}</p>
               </div>
             </div>
+          </div>
 
-            <div className="question-container-right">
-              <div className="question-container-top">
-                <div className="timestamp">
-                  <div className="timestamp-date">
-                    <small>
-                      {question.timestamp[0] +
-                        "/" +
-                        question.timestamp[1] +
-                        "/" +
-                        question.timestamp[2]}
-                    </small>
-                    <small>at</small>
-                  </div>
-                  <div className="timestamp-time">
-                    <small>
-                      {question.timestamp[3] + ":" + question.timestamp[4]}
-                    </small>
-                  </div>
+          <div className="question-container-right">
+            <div className="question-container-top">
+              <div className="timestamp">
+                <div className="timestamp-date">
+                  <small>
+                    {question.timestamp[0] +
+                      "/" +
+                      question.timestamp[1] +
+                      "/" +
+                      question.timestamp[2]}
+                  </small>
+                  <small>at</small>
                 </div>
-                <div className="question-tags">
-                  {question.tags.map((tag) => (
-                    <span className="tag">{tag}</span>
-                  ))}
+                <div className="timestamp-time">
+                  <small>
+                    {question.timestamp[3] + ":" + question.timestamp[4]}
+                  </small>
                 </div>
               </div>
-              <div className="question-body">
-                <div className="question-title">
-                  <Link>
-                    <p>{question.questionTitle}</p>
-                  </Link>
-                  </div>
-                  <div className="question-stats">
-                    <div className="question-stat">
-                      <p>{question.answerCount}</p>
-                      <span>answers</span>
-                    </div>
-                    <div className="question-stat">
-                      <p>{question.views}</p>
-                      <span>views</span>
-                    </div>
-                  </div>
+              <div className="question-tags">
+                {question.tags.map((tag) => (
+                  <span className="tag">{tag}</span>
+                ))}
+              </div>
+            </div>
+            <div className="question-body">
+              <div className="question-title">
+                <Link>
+                  <p>{question.questionTitle}</p>
+                </Link>
+              </div>
+              <div className="question-stats">
+                <div className="question-stat">
+                  <p>{question.answerCount}</p>
+                  <span>answers</span>
+                </div>
+                <div className="question-stat">
+                  <p>{question.views}</p>
+                  <span>views</span>
                 </div>
               </div>
             </div>
-          
-        ))}
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,11 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import QuestionItem from "../QuestionItem/QuestionItem";
 import "./Questions.css";
 
+const PAGE_SIZE = 10;
+
 const Questions = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [questions, setQuestions] = useState([]);
+  const totalPages = Math.ceil(questions.length / PAGE_SIZE);
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const currentContent = questions.slice(startIndex, endIndex);
+
+  const handlePrevPage = async() => {
+    
+
+const url = `https://queryus-production.up.railway.app/question/all?pageNo=${currentPage}`;
+try {
+  const res = await axios.get(url, { withCredentials: true });
+  console.log(res.data);
+  setQuestions(res.data);
+} catch (error) {
+  console.log(error);
+}
+if(currentPage>1){
+  setCurrentPage(currentPage+1);
+  console.log(currentContent());
+}
+  };
+
+  const handleNextPage = async() => {
+    const url = `https://queryus-production.up.railway.app/question/all?pageNo=${currentPage}`;
+
+    try {
+      const res = await axios.get(url, { withCredentials: true });
+      console.log(res.data);
+      setQuestions(res.data);
+    } catch (error) {
+      console.log(error);
+    } 
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://queryus-production.up.railway.app/question/all?pageNo=${currentPage}`;
+      try {
+        const res = await axios.get(url, { withCredentials: true });
+        console.log(res.data);
+        setQuestions(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [currentPage]);
+
   return (
     <div className="questions">
-      <QuestionItem />
+      {currentContent.map((question) => (
+        <QuestionItem key={question.id} question={question} />
+      ))}
+      <div className="pagination-container">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage >= totalPages}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
