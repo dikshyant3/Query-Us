0eee61a5277609448cb406782c91d83930b53e99
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-03-01 23:20:39 +0545

    token based authentication done

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -3,25 +3,27 @@ import axios from "axios";
 import QuestionItem from "../QuestionItem/QuestionItem";
 import "./Questions.css";
 
-
 const Questions = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [questions, setQuestions] = useState([]);
-  const token=localStorage.getItem("token");
-
+  const token = localStorage.getItem("token");
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       const url = `https://queryus-production.up.railway.app/question/all?pageNo=${currentPage}`;
       try {
-        const res = await axios.get(url,{
-          headers:{
-            'Authorization':`Bearer ${token}`,
-          }
-        }, { withCredentials: true });
+        const res = await axios.get(
+          url,
+          {
+            headers: headers,
+          },
+          { withCredentials: true }
+        );
         console.log(res.data);
         setQuestions(res.data);
-        
       } catch (error) {
         console.log(error);
       }
@@ -42,7 +44,7 @@ const Questions = () => {
       // }
     };
     fetchData();
-  }, [currentPage,token]);
+  }, [currentPage,token,headers]);
 
   const handlePrevPage = async () => {
     const url = `https://queryus-production.up.railway.app/question/all?pageNo=${
@@ -50,10 +52,8 @@ const Questions = () => {
     }`;
 
     try {
-      const res = await axios.get(url,{
-        headers:{
-          'Authorization':`Bearer ${token}`,
-        }
+      const res = await axios.get(url, {
+        headers: headers,
       });
       console.log(res.data);
       setQuestions(res.data);
@@ -68,9 +68,7 @@ const Questions = () => {
       currentPage + 1
     }`;
     try {
-      const res = await axios.get(url,{headers:{
-        'Authorization':`Bearer ${token}`
-      }} );
+      const res = await axios.get(url, { headers: headers });
       console.log(res.data);
       setQuestions(res.data);
       setCurrentPage(currentPage + 1);
@@ -85,11 +83,19 @@ const Questions = () => {
         <QuestionItem key={question.id} question={question} />
       ))}
       <div className="pagination-container">
-        <button className="btn-paginate" onClick={handlePrevPage} disabled={currentPage === 1}>
+        <button
+          className="btn-paginate"
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
           Previous
         </button>
         <div className="pageNo">{`Page ${currentPage}`}</div>
-        <button className="btn-paginate" onClick={handleNextPage} disabled={questions.length<10} >
+        <button
+          className="btn-paginate"
+          onClick={handleNextPage}
+          disabled={questions.length < 10}
+        >
           Next
         </button>
       </div>
