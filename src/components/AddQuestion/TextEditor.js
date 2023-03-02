c66657d7ba1fb3834937a5c7b03f5f9026cad392
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-03-03 00:22:52 +0545

    question post portion created

--- src/components/AddQuestion/TextEditor.js
+++ src/components/AddQuestion/TextEditor.js
@@ -1,47 +1,42 @@
-import React,{useState} from 'react'
-import './TextEditor.css'
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useState } from "react";
+import "./TextEditor.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-
-
-
-
-
-const  modules  = {
-    toolbar: [
-        [{ font: [] }],
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        ["bold", "italic", "underline", "strike"],
-        [{ color: [] }, { background: [] }],
-        [{ script:  "sub" }, { script:  "super" }],
-        ["blockquote", "code-block"],
-        [{ list:  "ordered" }, { list:  "bullet" }],
-        [{ indent:  "-1" }, { indent:  "+1" }, { align: [] }],
-        ["link", "image", "video"],
-        ["clean"],
-    ],
-    clipboard:{
-      matchVisual:false,
-    },
-    stripTags:true,
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
 };
-const TextEditor = ({handleQuestionText}) => {
-  const [value,setValue]=useState("");
+const TextEditor = ({ handleQuestionText }) => {
+  const [value, setValue] = useState("");
   // console.log(value);
-  const handleChange=(text)=>{
-    const plainText = text.replace(/<[^>]+>/g, '');
+  const handleChange = (text) => {
+    const plainText = text.replace(/<[^>]+>/g, "");
     setValue(plainText);
-    handleQuestionText(JSON.stringify(plainText));
-  
-
-  }
+    console.log(plainText);
+    handleQuestionText(plainText);
+  };
   return (
-    
-    <div className="quill-container">
-        <ReactQuill modules={modules} value={value} theme="snow" onChange={handleChange} placeholder="Content goes here..." />
+    <div className="quill-container" style={{ direction: "ltr" }}>
+      <ReactQuill
+        modules={modules}
+        value={value}
+        theme="snow"
+        onChange={handleChange}
+        placeholder="Content goes here..."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TextEditor
+export default TextEditor;
