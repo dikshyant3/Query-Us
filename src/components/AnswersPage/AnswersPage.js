a78cc9449494c00b5d6809b87a85299156ad8e83
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-03-08 23:42:46 +0545

    Creating comment section at answers page

--- src/components/AnswersPage/AnswersPage.js
+++ src/components/AnswersPage/AnswersPage.js
@@ -4,51 +4,65 @@ import React, { useState, useEffect } from "react";
 import "./AnswersPage.css";
 
 const AnswersPage = () => {
-  const token=localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const [question, setQuestion] = useState(null);
-  let search =window.location.search
-  const params=new URLSearchParams(search)
-  const id = params.get("query")
-  
+  const [show,setShow]=useState(false)
+  const [comment,setComment]=useState("")
+
+  // The below three lines of used to redirect a single question to answersPage
+  let search = window.location.search;
+  const params = new URLSearchParams(search);
+  const id = params.get("query");
+
   // console.log("DIKSHYANT")
   const url = `https://queryus-production.up.railway.app/question/${id}`;
 
   useEffect(() => {
-
     const fetchQuestion = async () => {
       try {
         const res = await axios.get(url, {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
         setQuestion(res.data);
-
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchQuestion();
   }, [token, url]);
 
   if (!question) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="answer-container">
       <div className="main-container">
-        <div className="question-header" key={question.id}>
-          <p>{question.questionTitle}</p>
-        </div>
-        <div className="question-main">
-          <p>{question.questionText}</p>
-        </div>
-        {question.answers.map((answer) => (
-          <div className="answer-main" key={answer.id}>
-            <p>{answer.answer}</p>
+        <div className="mainQuestion">
+          <div className="answersQuestionContainer">
+            <div className="question-header" key={question.id}>
+              <p>{question.questionTitle}</p>
+            </div>
+            <div className="question-main">
+              <p>{question.questionText}</p>
+            </div>
           </div>
-        ))}
+          <div className="questionButton">
+            <button className="questionBtn">Ask Question</button>
+          </div>
+        </div>
+        <div className="mainAnswer">
+          {question.answers.map((answer) => (
+            <div className="answer-main" key={answer.id}>
+              <p>{answer.answer}</p>
+            </div>
+          ))}
+        </div>
+        <div className="comments">
+          <p onClick={()=>setShow(!show)}></p>
+        </div>
       </div>
     </div>
   );
