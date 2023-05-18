3a2219b4dee58cf6f94e57cdba5100bbbc86e268
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-05-18 17:48:56 +0545

    Navbar completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,74 @@
-import React from 'react';
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 // import './Navbar.css';
-import Logo from '../../images/Logo2.png';
-import {GrSearch} from 'react-icons/gr';
-import {RiStarSLine} from 'react-icons/ri';
-import {BsPersonCircle} from 'react-icons/bs'
+import Logo from "../../images/Logo2.png";
+import { GrSearch } from "react-icons/gr";
+import { RiStarSLine } from "react-icons/ri";
+import { BsPersonCircle } from "react-icons/bs";
+import { Link } from "react-router-dom";
 // import StarsIcon from '@mui/icons-material/Stars';
 // import { Avatar } from '@mui/material';
 
-
-
 const Navbar = () => {
+  const [user,setUser]=useState({});
+  const token=localStorage.getItem('token');
+  useEffect(() => {
+    const fetchData = async () => {
+      const userUrl = `https://queryus-production.up.railway.app/user/me`;
+      try {
+        const res = await axios.get(userUrl,{
+          headers:{
+            'Authorization':`Bearer ${token}`,
+          }
+        });
+        console.log(res.data);
+        
+        setUser(res.data);
+        localStorage.setItem("currentUser",user)
+        console.log('getItem',localStorage.getItem('currentUser'))
+        
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [user,token]);
   return (
     <>
-        
-            <div className="flex justify-between items-center sticky top-0 h-[70px] z-10 cursor-pointer">
-                <img src={Logo} alt="Header-Logo" className='object-contain h-12 m-[20px]'/>
-                
-                <div className="flex items-center flex-1 min-w-fit p-[10px] h-[40px] rounded border-none outline-0 ring-0 bg-[#f5f5f5]">
-                    <GrSearch style={{fill:'slate'}}/>
-                    <input type="text" placeholder='Search' className='w-full h-[40px] outline-none border-0 ring-0 focus:border-0 focus:ring-0 bg-[#f5f5f5]'/>
-                </div>
-                <div className="flex items-center justify-between">
-                    {/* Reputation */}
-                    {/* <StarsIcon style={{fill:"#fff",opacity:0.8}}/> */}
-                    <RiStarSLine/>
+      <div className="flex justify-between items-center sticky top-0 h-[70px] z-10 cursor-pointer ">
+        <img
+          src={Logo}
+          alt="Header-Logo"
+          className="object-contain h-12 m-[20px] mr-8"
+        />
 
-                    {/* UserProfile */}
-                    <BsPersonCircle/>
-                    {/* <Avatar sx={{width:24,height:24}}/> */}
-                </div>
-            </div>
-        
+        <div className="flex items-center w-[60%] p-[10px] h-[40px] rounded border-none outline-0 ring-0 bg-[#f5f5f5]">
+          <GrSearch style={{ fill: "slate" }} />
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full h-[40px] outline-none border-0 ring-0 focus:border-0 focus:ring-0 bg-[#f5f5f5]"
+          />
+        </div>
+        <div className="flex items-center justify-between ml-4">
+          {/* Reputation */}
+          {/* <StarsIcon style={{fill:"#fff",opacity:0.8}}/> */}
+          <div className="pr-6 flex items-center">
+            <RiStarSLine className="h-6 w-6" />
+            <p className="px-2 text-indigo-700 font-bold">{user.reputation}</p>
+          </div>
+          {/* UserProfile */}
+          <div className="mr-10">
+            <Link to="/userProfile" className="flex text-black cursor-pointer">
+              <p className="px-2 text-indigo-700 font-bold">{user.firstName}</p>
+              <BsPersonCircle className="h-6 w-6" />
+            </Link>
+          </div>
+          {/* <Avatar sx={{width:24,height:24}}/> */}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
