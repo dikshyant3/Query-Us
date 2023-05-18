ea1559d92a7439908a496e7c8d42d60d6666c5cb
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-05-18 15:33:03 +0545

    fixed the answerUpvote bug

--- src/components/AnswersPage/AnswerUpvote.js
+++ src/components/AnswersPage/AnswerUpvote.js
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
-import { GoTriangleUp } from "react-icons/go";
+import { BsTriangleFill } from "react-icons/bs";
 import axios from "axios";
 
-const AnswerUpvote = ({ count,id }) => {
+const AnswerUpvote = ({ upVoted,count,id }) => {
   const token = localStorage.getItem("token");
   const [voteCount, setVoteCount] = useState(count);
-  const [isUpvoted, setIsUpvoted] = useState(false);
+  const [isUpvoted, setIsUpvoted] = useState(upVoted);
 
   //  Answer Upvote URL
   const url = `https://queryus-production.up.railway.app/vote/answer/${id}`;
 
   const handleUpvote = async () => {
-    if (!isUpvoted) {
+    
       try {
-        await axios.post(url, {
+        await axios.post(url,null, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
+        if (!isUpvoted) {
         setVoteCount(voteCount + 1);
         setIsUpvoted(true);
-      } catch (error) {
-        console.log(error);
+      } 
+      else{
+        setVoteCount(voteCount-1);
+        setIsUpvoted(false);
       }
     }
+    catch (error) {
+        console.log(error);
+      }
+    
   };
   return (
     <div className="flex flex-col items-center">
-      <button type="button" onClick={handleUpvote}>
-        <GoTriangleUp className={`${isUpvoted}?text-gray-400:text-green-500`}/>
+      <button type="button" className={`text-xl ${isUpvoted ? 'text-green-500' : 'text-gray-400'}`} onClick={handleUpvote}>
+        <BsTriangleFill />
       </button>
       <p>{voteCount}</p>
     </div>
