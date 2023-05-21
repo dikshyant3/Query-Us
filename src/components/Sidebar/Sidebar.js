b4653fd8903b7f5ad52a6deec1c207ef802a89e4
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-05-21 13:55:48 +0545

    Sidebar modified

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -31,16 +31,16 @@ const Sidebar = () => {
     fetchData();
   }, [token])
   return (
-    <div className="flex flex-col sticky top-0 w-[350px] bg-[#f5f5f5] border-r-2 border-gray-300">
-      <div className="mx-auto mt-6 mb-4 ">
+    <div className="flex flex-col items-center sticky top-0 w-[350px] bg-[#f5f5f5] border-r-2 border-gray-300">
+      <div className=" mt-6 mb-4 ">
         <Link to="/addquestion">
-          <button className="px-[10px] py-[8px] bg-indigo-500 text-white text-base rounded-xl cursor-pointer">
+          <button className="px-[12px] py-[8px] bg-indigo-500 text-white text-lg rounded-xl cursor-pointer">
             Ask a Question
           </button>
         </Link>
       </div>
       <div className="flex items-center my-6 ">
-        <div className="flex flex-col mx-auto">
+        <div className="flex flex-col">
           <div className="flex items-start">
             <AiFillHome className="text-xl text-indigo-500"/>
             <Link to="/" className="pl-2 opacity-60 hover:opacity-100">Home</Link>
@@ -51,7 +51,7 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center mx-auto">
+      <div className="flex flex-col items-center">
         <h3 className="text-xl font-semibold text-indigo-600">Popular Tags</h3>
         <div className="">
           {tags.map(tag=>(
